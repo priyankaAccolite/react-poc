a6b02fcec1c98ea40aec7c64df80379dc7774411
Shivangi Asthana <shivangiasthana@Administrators-MacBook-Pro.local>
2022-11-30 17:15:52 +0530

    date picker changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Button from "./Components/Button";
 import MainContainer from "./MainContainer";
+import Headers from './Components/Header'
+import DateInput from "./Components/DateInput";
 
 function App() {
   return (
     <div className="App">
       <MainContainer />
       <Button name="Add to list" />
+      <Headers title={"Product Details"}/>
+      <DateInput/>
     </div>
   );
 }
