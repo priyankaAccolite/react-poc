65d99b6e2267b74db4ba810a588d930fd7334d4b
Shivangi Asthana <shivangiasthana@Administrators-MacBook-Pro.local>
2022-11-30 12:24:45 +0530

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's Learn React together
         </a>
       </header>
     </div>
