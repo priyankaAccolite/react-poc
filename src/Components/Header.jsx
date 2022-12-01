20657710440441c453775100ee6e51d8478990b1
priyankaAccolite <priyanka.p@accolitedigital.com>
2022-12-01 17:23:23 +0530

    merge conflicts resoved

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import "../Styles/header.css"
+import "../Styles/header.css";
 
 const Header = (props) => {
-  return <div className="headerView" style={
-    {border: '1px solid black'}
-    }>
-  <div style={{marginTop:5}}><text className="header" >{props.title}</text></div>
-  </div>
+  return (
+    <div className="headerView" style={{ border: "1px solid black" }}>
+      <div style={{ marginTop: 5 }}>
+        <h1 className="header">{props.title}</h1>
+      </div>
+    </div>
+  );
 };
 export default Header;
--- src/Containers/ProductDetails.jsx
+++ src/Containers/ProductDetails.jsx
@@ -1,23 +1,38 @@
 import React from "react";
 import Header from "../Components/Header";
-import productDetails from '../Constants/Constants'
+import productDetails from "../Constants/Constants";
 
 const ProductDetails = () => {
-  return <>
-  <Header title={"Product Details"}/>
-  {productDetails.map((item)=>{
-    console.log("ProductDetails",productDetails)
-    return <>
-    <div style={{flexDirection:'row', justifyContent:'space-between', display:'flex', padding:20, marginRight:20, marginLeft:20}}>
-      <text>* {item.label}</text>
-      {(item.type).map((i)=>{
-        return <>
-        <text>{i.placeHolder}</text>
-        </>
+  return (
+    <>
+      <Header title={"Product Details"} />
+      {productDetails.map((item) => {
+        console.log("ProductDetails", productDetails);
+        return (
+          <>
+            <div
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                display: "flex",
+                padding: 20,
+                marginRight: 20,
+                marginLeft: 20,
+              }}
+            >
+              <h1>* {item.label}</h1>
+              {item.type.map((i) => {
+                return (
+                  <>
+                    <h1>{i.placeHolder}</h1>
+                  </>
+                );
+              })}
+            </div>
+          </>
+        );
       })}
-    </div>
     </>
-  })}
-  </>;
+  );
 };
 export default ProductDetails;
