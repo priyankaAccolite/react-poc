eee4992da9412e27c5f612034e755bf5712508d2
priyankaAccolite <priyanka.p@accolitedigital.com>
2022-12-01 15:13:24 +0530

    dropdown-final-changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import "./App.css";
 import Button from "./Components/Button";
-import DropDown from "./Components/DropDown";
 import MainContainer from "./MainContainer";
 import React from "react";
-const options = [
-  { value: "C++", label: "C++" },
-  { value: "JAVA", label: "JAVA" },
-  { value: "Javascript", label: "Javascript" },
-  { value: "Python", label: "Python" },
-  { value: "Swift", label: "Swift" },
-];
+import DropDown from "./Components/DropDown";
 
 function App() {
   const [selectedval, setSelectedVal] = React.useState("");
   const handleChange = (e) => {
-    setSelectedVal(e.target.value);
+    setSelectedVal(e.value);
   };
 
   return (
     <div className="App">
       <MainContainer />
       <Button name="Add to list" />
-      <DropDown options={options} handleChange={handleChange} />
+      <br />
+
+      <DropDown handleChange={handleChange} />
     </div>
   );
 }
--- src/Components/DropDown.jsx
+++ src/Components/DropDown.jsx
@@ -1,18 +1,68 @@
 import React from "react";
 import "../Styles/dropdown.css";
+import Select from "react-select";
+const options = [
+  { value: "C++", label: "C++" },
+  { value: "JAVA", label: "JAVA" },
+  { value: "Javascript", label: "Javascript" },
+  { value: "Python", label: "Python" },
+  { value: "Swift", label: "Swift" },
+];
 
-const DropDown2 = ({ handleChange, options }) => {
+const DropDown = ({ handleChange }) => {
+  function customTheme(theme) {
+    return {
+      ...theme,
+      colors: {
+        ...theme.colors,
+        primary25: "orange",
+        primary: "grey",
+        primary50: "#eeefee",
+      },
+    };
+  }
+  const styles = {
+    option: (provided, state) => {
+      return {
+        ...provided,
+        fontWeight: "normal",
+        color: "black",
+        backgroundColor: "white",
+        padding: "6px 6px",
+        fontSize: "14px",
+      };
+    },
+
+    control: (styles) => {
+      return {
+        ...styles,
+        height: "10px",
+        minHeight: 35,
+        borderColor: "black",
+      };
+    },
+  };
+
   return (
-    <div>
-      <select className="select" name="cars" onChange={handleChange}>
-        <option style={{ display: "none" }}></option>
-        {options?.map((item) => (
-          <option value={item.value} key={item.value}>
-            {item.value}
-          </option>
-        ))}
-      </select>
+    <div
+      style={{
+        width: "200px",
+        display: "inline-block",
+        justifyContent: "center",
+      }}
+    >
+      <Select
+        theme={customTheme}
+        myFontSize="20px"
+        options={options}
+        styles={styles}
+        placeholder=""
+        components={{
+          IndicatorSeparator: () => null,
+        }}
+        onChange={handleChange}
+      />
     </div>
   );
 };
-export default DropDown2;
+export default DropDown;
