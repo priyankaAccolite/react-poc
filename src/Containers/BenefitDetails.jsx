012fbfc47d63031939d3959957ba846fb5a02e12
Vijayakumar-Accolite <vijayakumar.r@accolitedigital.com>
2022-12-06 18:28:59 +0530

    benefit details container modified

--- src/Containers/BenefitDetails.jsx
+++ src/Containers/BenefitDetails.jsx
@@ -1,44 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import Header from "../Components/Header";
-import {benefitDetails} from "../Constants/Constants";
+import {benefitDetails, otherLaValidations} from "../Constants/Constants";
 import DropDown from "../Components/DropDown";
 import '../Styles/productDetails.css'
 import TextboxWithRadio from "../Components/TextboxWithRadio";
-import { currencyCode } from '../Constants/Constants';
-import TextInput from '../Components/TextInput'
 import FileUpload from "../Components/FileUpload";
 import { applicableBenefits } from "../Constants/Constants";
+import TextInput from "../Components/TextInput";
 
 const BenefitDetails = () => {
+
+  const [textValue,setTextValue]=useState('');
   const [value, setValue] = useState('');
   const [rerender, Setrerender] = useState(false);
+  const [display,setDisplay]=useState(true);
 
+  const handleTextChange=(e)=>{
+     setTextValue(e.target.value);
+  };
   const handleChange = (e) => {
     setValue(e.label);
     Setrerender(!rerender);
-    //console.log("selected-value",value);
   };
-  const handleClick = () => {
-    if(!applicableBenefits.includes(value))
-    applicableBenefits.push(value);
-    Setrerender(!rerender);
-    //console.log("global-array",currencyCode);
+  const handleClick = (e) => {
+    if(e.target.dataset.id){
+      let arrayValue=`${value}-${textValue}`;
+      if(!otherLaValidations.includes(arrayValue))
+      otherLaValidations.push(arrayValue);
+      Setrerender(!rerender);
+      setTextValue("");
+    }
+    else{
+      if(!applicableBenefits.includes(value))
+      applicableBenefits.push(value);
+      Setrerender(!rerender);
+    }
   };
   const handleEdit=()=>{
 
   };
+  const handleAccordion=()=>{
+    setDisplay(!display);
+};
+
+
 
   return (
     <>
+       <div style={{display:"flex",flexDirection:"row",justifyContent:"space-between"}}>
       <Header title={"Benefit Details"} />
+      <div >
+            <input type="button" onClick={handleAccordion} style={{padding:12,width:60,backgroundColor:"white",borderLeft: '1px solid black', borderBottom: '1px solid black', borderTop: 'none', borderRight: 'none'}} value="-"/>
+      </div>
+    </div>
       {benefitDetails.map((item) => {
         return (
           <>
             <div
               style={{
                 flexDirection: "row",
-                display: "flex",
+                display:display?"flex":"none",
                 padding: 12,
+                marginRight:20,
+                marginLeft:20,
               }}
             >
               <div style={{ width: 400, textAlign: 'left', fontSize: 16 }}>{item.id + ".  " + item.label}</div>
@@ -58,6 +82,13 @@ const BenefitDetails = () => {
                 } else if (i.placeHolder === "button") {
                   return <div style={{ marginLeft: 10 }}>
                     <input type="button" value="Add to List" onClick={handleClick} style={{border:'1px solid black', backgroundColor:"#edb525", height:35, borderRadius:3 ,width:75, fontWeight:'bold', fontSize:12}}/>
+                  </div>
+                } 
+                else if (i.placeHolder === "textinputlist") {
+                  return <div style={{ display:"flex", flexDirection:"row",marginLeft: 10 }}>
+                    <TextInput size="20" value={textValue} handleChange={handleTextChange}/>
+                    <input type="button" data-id={item.id} value="Add to List" onClick={handleClick} style={{border:'1px solid black', backgroundColor:"#edb525", height:35, borderRadius:3 ,width:75, fontWeight:'bold', fontSize:12}}/>
+                    <TextboxWithRadio currencyCode={otherLaValidations} />
                   </div>
                 } 
                 else {
