d60d071e2a93b461892aa9d1e9e775e21312a984
Vijayakumar-Accolite <vijayakumar.r@accolitedigital.com>
2022-12-06 18:26:13 +0530

    main container changes done

--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -3,10 +3,11 @@ import ProductDetails from './Containers/ProductDetails';
 import BenefitDetails from './Containers/BenefitDetails';
 import "./Styles/mainContainer.css";
 import { Container } from 'react-bootstrap';
+import PolicyServicing from "./Containers/PolicyServicing";
 
 
 const MainContainer = () => {
-  return <div style={{ margin: '20px' }}>
+  return <div>
     <Container style={{ width: '100%', paddingTop: '10px', paddingBottom: '10px' }}>
       <div className="mainContainer">
         <div align="left" style={{ paddingLeft: '10px', fontWeight: 'bold' }}>
@@ -15,6 +16,7 @@ const MainContainer = () => {
         <hr style={{ border: '1px solid gray' }} />
         <div className="subContainer"><ProductDetails /></div>
         <div className="subContainer"><BenefitDetails /></div>
+        <div className="subContainer"><PolicyServicing /></div>
       </div>
     </Container>
   </div>;
