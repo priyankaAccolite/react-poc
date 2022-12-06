3432199c8dded10542ae047ef7a1119834d85556
Vijayakumar-Accolite <vijayakumar.r@accolitedigital.com>
2022-12-06 18:28:32 +0530

    textinput state changes done

--- src/Components/TextInput.jsx
+++ src/Components/TextInput.jsx
@@ -2,14 +2,9 @@ import React, { useState } from "react";
 
 //pass size as props from wherever you are using this component
 const TextInput = (props) => {
-    const [value, setValue] = useState("");
-
-    const handleChange = (e) => {
-        setValue(e.target.value);
-    };
-    
+  
     return (
-        <input style={{border:'1px solid black', height:30, borderRadius:3}} type="text" value={value} onChange={e => handleChange(e)} size={props.size} />
+        <input style={{border:'1px solid black', height:30, borderRadius:3}} type="text" value={props.value} onChange={e => props.handleChange(e)} size={props.size} />
     );
 };
 
